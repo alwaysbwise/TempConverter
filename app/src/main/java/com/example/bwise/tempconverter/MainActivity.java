138787f8536731a35c65161c6ccd662e107789f0
alwaysbwise <33883062+alwaysbwise@users.noreply.github.com>
2018-01-24 19:06:25 -0800

    Infer Constraints + font color change for temperature labels Add Java to compute NOT FUNCTIONING AS EXPECTED output constant

--- app/src/main/java/com/example/bwise/tempconverter/MainActivity.java
+++ app/src/main/java/com/example/bwise/tempconverter/MainActivity.java
@@ -2,15 +2,60 @@ package com.example.bwise.tempconverter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import java.text.NumberFormat;
+
+public class MainActivity extends Activity
+        implements TextView.OnEditorActionListener {
+    // define widget variable
+    private EditText inputeditText;
+    private TextView outputtextView;
+
+    //define instance variables - not saved
+    private String temperatureValueString = "";
+   //private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get references to widgets
+        inputeditText = findViewById(R.id.inputeditText);
+        outputtextView = findViewById(R.id.outputtextView);
+
+        //listener
+        inputeditText.setOnEditorActionListener(this);
     }
 
-        //c=(F-32)*5/9
+    @Override
+    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+        calculateAndDisplay();
+        return false;
+    }//end onEditorAction
 
-}
+    public void calculateAndDisplay() {
+        //c=(F-32)*5/9
+        //take user entry convert/parse to float
+       temperatureValueString = inputeditText.getText().toString();
+       float temperatureValue;
+      // float celsiusTemp;
+
+
+       if (temperatureValueString.equals("")){
+           temperatureValue = 0;
+       }
+       else {
+           temperatureValue = Float.parseFloat(temperatureValueString);
+       }
+       //calculate temperature
+        float celsiusTemp = (temperatureValue - 32) * (5/9);
+        //display
+        //NumberFormat numberFormat = NumberFormat.getNumberInstance();
+        outputtextView.setText(String.valueOf(celsiusTemp));
+
+    } //end calculateAndDisplay method 
+}//end main activity
